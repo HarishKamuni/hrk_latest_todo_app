b86e653eaa7fd1c2bb94ad9193b0dad398a891d7
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-08-21 12:05:02 +0530

    Added Redux-toolkit and created store and todoSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TodoApp from './components/TodoApp';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const App = () => {
   return (
     <>
-      <div>
+      <Provider store={store}>
         <TodoApp />
-      </div>
+      </Provider>
     </>
   );
 };
